b1e2413b7a8cea3cef366d1c9569dbbe13b0e7b7
yakinrei <57528602+yakinrei@users.noreply.github.com>
2021-03-24 14:45:39 +0000

    app
    Jogo

---
+++ offline.js
@@ -0,0 +1,33 @@
+﻿{
+	"version": 1616597093,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/parallaxmountainbg-sheet0.png",
+		"images/parallaxmountainmontainfar-sheet0.png",
+		"images/parallaxmountainmountains-sheet0.png",
+		"images/parallaxmountainforegroundtrees-sheet0.png",
+		"images/parallaxmountaintrees-sheet0.png",
+		"images/idle__-sheet0.png",
+		"images/idle__-sheet1.png",
+		"images/idle__2-sheet0.png",
+		"images/idle__2-sheet1.png",
+		"images/idle__2-sheet2.png",
+		"images/run__-sheet0.png",
+		"images/jump__-sheet0.png",
+		"images/jump__-sheet1.png",
+		"images/block.png",
+		"images/teto.png",
+		"images/realplayer-sheet0.png",
+		"media/8-bit-ninja.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"cordova.oauth2.js"
+	]
+}
